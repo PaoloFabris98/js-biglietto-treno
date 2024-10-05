b3fa7057ebe7de52b11b24aadf704849ed8ca700
PaoloFabris98 <f98paolo@gmail.com>
2024-10-05 17:20:36 +0200

    feat: completed

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
+let km = prompt("Chilometri da percorrere:");
+console.log(km);
+let età = prompt("Età del passeggero:");
+console.log(età);
+let cost = 0.21;
+let discount;
+let price;
 
-do {
+if (età < 18) {
+    discount = ((km*cost)/100)*20;
+    price = (km*cost) - discount;
+    console.log(price);
+} else if (età > 65) {
+    discount = ((km*cost)/100)*40;
+    price = (km*cost) - discount;
+    console.log(price);
+} else {
+    price = km*cost;
+    console.log(price);
+};
 
-    let exit = false
-    let km = prompt("Chilometri da percorrere:");
-    console.log(km);
-    let età = prompt("Età del passeggero:");
-    console.log(età);
-    
+ document.getElementById("output").innerHTML = `
+    <p><strong>Chilometri:</strong> ${km}</p>
+    <p><strong>Costo al KM:</strong> ${cost}€</p>
+    <p><strong>Età:</strong> ${età}</p>
+    <p><strong>Prezzo biglietto:</strong> ${price.toFixed(2)}€</p>
+    `;
 
-} while (exit != false);
