7171a1960527738f4333c8c13fe4ffc2aa2b2c17
PaoloFabris98 <f98paolo@gmail.com>
2024-10-05 16:55:21 +0200

    feat: start

---
+++ main.js
@@ -0,0 +1,11 @@
+
+do {
+
+    let exit = false
+    let km = prompt("Chilometri da percorrere:");
+    console.log(km);
+    let età = prompt("Età del passeggero:");
+    console.log(età);
+    
+
+} while (exit != false);
